ac03d76f366a62e48fa2b3a8e485f2ffe6510c3d
karlaprzbr <karla.perezbrugues@gmail.com>
2019-03-20 12:33:02 +0100

    add start files

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+    <head>
+        <title>VENTE EN LIGNE DE VETEMENTS</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </head>
+    <body>
+        <header><img src="img/logo_portfolio.PNG" alt="logo"/></header>
+        <nav>
+            <ul>
+                <li><a href="#">FEMMES</a></li>
+                <li><a href="#">HOMMES</a></li>
+                <li><a href="#">ENFANTS</a></li>
+                <li><a href="#">PROMOTIONS</a></li>
+            </ul>
+            <div id="icons">
+                <a href="#"><i class="fas fa-user"></i></a>
+                <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                <a href="#"><i class="fas fa-search"></i></a>
+            </div>
+        </nav>
+        
+        <div id="sideNav">
+            <div id="contenu">
+                <p>CATALOGUE PAR PRODUITS</p>
+                <a href="#">Tops</a>
+                <a href="#">Jeans</a>
+                <a href="#">Robes</a>
+                <a href="#">Vestes</a>
+                <a href="#">Chaussures</a>
+                <p>CATALOGUE PAR CONCEPT</p>
+                <a href="#">Basics</a>
+                <a href="#">Denim</a>
+                <a href="#">Habillé</a>
+            </div>
+        </div>
+
+        <div id="main">
+        </div>
+        
+        <footer></footer>
+        
+    </body>
+</html>
