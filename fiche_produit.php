f8f01a2116c33c6ee42dfeca56b9373f0ef81914
karlaprzbr <karla.perezbrugues@gmail.com>
2019-05-24 01:32:56 +0200

    add minor changes

--- fiche_produit.php
+++ fiche_produit.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
+require_once("includes/script.php");
 include("fonctions_panier.php");
-// var_dump(include "fonctions_panier.php");
-// exit();
-$bdd = new PDO('mysql:host=localhost;dbname=fringuesvp','root','');
-$bdd->exec("SET NAMES 'UTF8'");
+
 if(isset($_GET['pdt_id']) AND !empty($_GET['pdt_id'])) {
-  $get_id = strip_tags($_GET['pdt_id']);
-  $produit = $bdd->prepare('SELECT * FROM produits INNER JOIN images ON produits.pdt_img_id = images.img_id INNER JOIN types_vet ON produits.pdt_type_vet_id = types_vet.type_vet_id INNER JOIN genres_vet ON produits.pdt_genre_vet_id = genres_vet.genre_vet_id INNER JOIN membres ON produits.pdt_membre_id = membres.membre_id WHERE pdt_id =' . $get_id);
-  $produit->execute(array($get_id));
+  $pdt_id = strip_tags($_GET['pdt_id']);
+  $produit = $bdd->prepare('SELECT * FROM produits INNER JOIN images ON produits.pdt_img_id = images.img_id INNER JOIN types_vet ON produits.pdt_type_vet_id = types_vet.type_vet_id INNER JOIN genres_vet ON produits.pdt_genre_vet_id = genres_vet.genre_vet_id INNER JOIN membres ON produits.pdt_membre_id = membres.membre_id WHERE pdt_id =' . $pdt_id);
+  $produit->execute(array($pdt_id));
   $donnees = $produit->fetch();
-  $pdt_id = $_GET['pdt_id'];
   $pdt_libelle = $donnees['pdt_libelle'];
   $pdt_prix = $donnees['pdt_prix'];
   // var_dump($pdt_libelle);
@@ -44,7 +41,7 @@ if(isset($_GET['pdt_id']) AND !empty($_GET['pdt_id'])) {
             <p><?php echo $donnees['pdt_description'] ?> </p>
             <p>
               <?php echo $donnees['pdt_prix'] ?>€
-              <form action="fiche_produit.php?pdt_id=<?= $donnees['pdt_id'] ?>" method="POST"><input type="submit" name="ajout_panier" value="Ajouter au panier"></form>
+              <form action="fiche_produit.php?pdt_id=<?= $_GET['pdt_id'] ?>" method="POST"><input type="submit" name="ajout_panier" value="Ajouter au panier"></form>
               <?php
               if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['ajout_panier'])) {
                 ajouterArticle($pdt_id,$pdt_libelle,$pdt_prix);
--- fonctions_panier.php
+++ fonctions_panier.php
@@ -13,16 +13,19 @@ function ajouterArticle($pdt_id,$pdt_libelle,$pdt_prix) {
   // Si le panier existe
   if (isset($_SESSION['panier'])) {
     // On regarde si le produit est déjà dans le panier
-    //$position_pdt = array_search($pdt_libelle, $_SESSION['panier']['pdt_libelle']);
-    //if ($position_pdt == false) {
-      // S'il n'est pas déjà dans le panier alors on l'ajoute
+    $position_pdt = array_search($pdt_id, $_SESSION['panier']['pdt_id']);
+    if ($position_pdt !== false) {
+      // S'il est déjà dans le panier on le notifie au client
+      echo "<p>L'article est déjà dans le panier.</p>";
+    } else {
+      // Sinon on l'ajoute
       array_push($_SESSION['panier']['pdt_id'], $pdt_id);
       array_push($_SESSION['panier']['pdt_libelle'], $pdt_libelle);
       array_push($_SESSION['panier']['pdt_prix'], $pdt_prix);
-    //}
+      echo "<p>L'article a été ajouté au panier.</p>";
+    }
   } else {
     creationPanier();
-    // echo "Un problème est survenu veuillez contacter l'administrateur du site.";
   }
 }
 
@@ -42,7 +45,7 @@ function supprimerArticle($pdt_libelle) {
         array_push($tmp['pdt_prix'], $_SESSION['panier']['pdt_prix'][$i]);
       }
     }
-    // On remplace notre panier session par le panier temp
+    // On remplace notre panier session par le panier temporaire
     $_SESSION['panier'] = $tmp;
     // On efface le panier temporaire
     unset($tmp);
@@ -52,7 +55,7 @@ function supprimerArticle($pdt_libelle) {
 }
 
 function MontantGlobal() {
-  $total = 0;
+  $total = floatval(0);
   for ($i = 0; $i < count($_SESSION['panier']['pdt_libelle']); $i++) {
     $total += $_SESSION['panier']['pdt_prix'][$i];
   }
